dd1d958f6743d68f29bccb2e49edab947823d351
desireKonan <harveykonan@gmail.com>
2024-03-28 13:04:45 +0100

    fix: correction du router

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -9,14 +9,11 @@ const FullLayout = Loadable(lazy(() => import('../layouts/full/FullLayout')));
 /* ****Pages***** */
 const Modern = Loadable(lazy(() => import('../views/dashboard/Modern')));
 const ListRegion = Loadable(lazy(() => import('../views/parameter/ville/VilleList')));
-const FormulaireRegion = Loadable(lazy(() => import('../views/parameter/ville/VilleForm')));
 
 const ListeAssemblee = Loadable(lazy(() => import('../views/parameter/assembly/AssemblyList')));
-const FormulaireAssemblee = Loadable(lazy(() => import('../views/parameter/assembly/AssemblyForm')));
 const ListeMembre = Loadable(lazy(() => import('../views/effective/member/MembreList')));
 
 const ListeTypeActivite = Loadable(lazy(() => import('../views/parameter/type-activite/TypeActiviteList')));
-const FormulaireTypeActivite = Loadable(lazy(() => import('../views/parameter/type-activite/TypeActiviteForm')));
 
 const ListeTypeRencontre = Loadable(lazy(() => import('../views/rencontre/TypeRencontreList')));
 const ListeRencontre = Loadable(lazy(() => import('../views/rencontre/RencontreList')));
@@ -28,8 +25,6 @@ const ListeRubriques = Loadable(lazy(() => import('../views/finance/RubriqueList
 const ListeLigneFinancieres = Loadable(lazy(() => import('../views/finance/LigneFinanciereListe'))); 
 
 const ListeActivites = Loadable(lazy(() => import('../views/parameter/activite/ActiviteList')));
-const FormulaireActivite = Loadable(lazy(() => import('../views/parameter/activite/ActiviteForm')));
-
 const ListeParticipants = Loadable(lazy(() => import('../views/parameter/activite/participants/ParticipantList')));
 
 const Router = [
@@ -40,18 +35,10 @@ const Router = [
       { path: '/', element: <Navigate to="/dashboard" /> },
       { path: '/dashboard', exact: true, element: <Modern /> },
       { path: '/regions', exact: true, element: <ListRegion /> },
-      { path: '/region', exact: true, element: <FormulaireRegion />},
       { path: '/type-activites', exact: true, element: <ListeTypeActivite />},
-      { path: '/type-activite', exact: true, element: <FormulaireTypeActivite />},
-      { path: '/type-activite/:id', exact: true, element: <FormulaireTypeActivite />},
       { path: '/activites', exact: true, element: <ListeActivites />},
       { path: '/activite/:id/participants', exact: true, element: <ListeParticipants />},
-      { path: '/activite', exact: true, element: <FormulaireActivite />},
-      { path: '/activite/:id', exact: true, element: <FormulaireActivite />},
-      { path: '/region/:id', exact: true, element: <FormulaireRegion />},
       { path: '/assemblees', exact: true, element: <ListeAssemblee />},
-      { path: '/assemblee', exact: true, element: <FormulaireAssemblee />},
-      { path: '/assemblee/:id', exact: true, element: <FormulaireAssemblee />},
       { path: '/assemblee/:id/membre', exact: true, element: <ListeMembre />},
       { path: '/type-rencontres', exact: true, element: <ListeTypeRencontre />},
       { path: '/rencontres', exact: true, element: <ListeRencontre />},
